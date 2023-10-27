04ea139fb7312bcd0e2c7500d62cff0f9f627b41
KendallFdez <kenfernandez204@gmail.com>
2023-10-27 16:00:58 -0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module proyecto2.proyecto2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires net.synedra.validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+            requires com.almasb.fxgl.all;
+    
+    opens proyecto2.proyecto2 to javafx.fxml;
+    exports proyecto2.proyecto2;
+}
